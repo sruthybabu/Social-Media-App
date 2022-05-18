853a0791f5f2fc5829a3853e1a9210c8043dcbe3
sruthybabu <sruthybabu64@gmail.com>
2022-05-18 20:38:38 +0530

    completed

--- src/components/Searchpost.js
+++ src/components/Searchpost.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './Navbar'
 
 const Searchpost = () => {
+    var [title,setTitle]=useState("")
+    const subData=()=>{
+        const data={"title":title}
+        console.log(data)
+    }
   return (
     <div>
+        <Navbar/>
 <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="row g-3">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" className="form-label">Title</label>
-                    <input placeholder="Enter title" type="text" className="form-control"/>
+                    <input onChange={(e)=>{setTitle(e.target.value)}} placeholder="Enter title" type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <button className="btn btn-primary">SEARCH</button>
+                    <button onClick={subData} className="btn btn-primary">SEARCH</button>
                 </div>
             </div>
         </div>
