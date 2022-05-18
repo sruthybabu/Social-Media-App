8ea3aadef00871c98f99a2498d9084584f54e1e1
sruthybabu <sruthybabu64@gmail.com>
2022-05-18 14:09:10 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addpost from './components/Addpost';
 import Searchpost from './components/Searchpost';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-<Addpost/>
-<Searchpost/>
+<BrowserRouter>
+<Routes>
+  <Route path="/" exact element={<Addpost/>}/>
+  <Route path="/search" exact element={<Searchpost/>}/>
+</Routes>
+
+</BrowserRouter>
 
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+
+<nav class="navbar navbar-expand-lg  navbar-dark bg-primary">
+  <div class="container-fluid">
+      <Link to="/" class="navbar-brand">Social Media</Link>
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+          <Link to="/" class="nav-link active">Add Post</Link>
+        <Link to= "/search" class="nav-link" >Search Post</Link>
+       
+      </div>
+    </div>
+  </div>
+</nav>
+
+    </div>
+  )
+}
+
+export default Navbar
