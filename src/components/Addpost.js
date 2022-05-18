563e53d28de4823683a3e4a7a72527ce5d3902ae
sruthybabu <sruthybabu64@gmail.com>
2022-05-18 20:26:34 +0530

    complete

--- src/components/Addpost.js
+++ src/components/Addpost.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const Addpost = () => {
+var [title,setTitle]=useState("")
+var [message,setMessage]=useState("")
+
+const subData=()=>{
+    const data={"title":title,"message":message}
+    console.log(data)
+}
+
   return (
     <div>
 <Navbar/>
@@ -11,14 +19,14 @@ const Addpost = () => {
             <div className="row g-3">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" className="form-label">Title</label>
-                    <input placeholder="Enter Title" type="text" className="form-control"/>
+                    <input onChange={(e)=>{setTitle(e.target.value)}} placeholder="Enter Title" type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" className="form-label">Message</label>
-                    <textarea placeholder="Enter message" name="" id="" cols="30" rows="10" className="form-control"></textarea>
+                    <textarea onChange={(e)=>{setMessage(e.target.value)}} placeholder="Enter message" name="" id="" cols="30" rows="10" className="form-control"></textarea>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <button className="btn btn-primary">SUBMIT</button>
+                    <button onClick={subData} className="btn btn-primary">SUBMIT</button>
                 </div>
             </div>
         </div>
